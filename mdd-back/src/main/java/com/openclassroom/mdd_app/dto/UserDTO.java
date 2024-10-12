2b6f6d2c631e3f20079015f8e2029565cfade911
Manuel <manuelfamiliar@laposte.net>
2024-10-12 14:20:19 +0200

    Connexion User

--- mdd-back/src/main/java/com/openclassroom/mdd_app/dto/UserDTO.java
+++ mdd-back/src/main/java/com/openclassroom/mdd_app/dto/UserDTO.java
@@ -1,37 +1,50 @@
-// package com.openclassroom.mdd_app.dto;
+package com.openclassroom.mdd_app.dto;
 
-// import jakarta.validation.constraints.Email;
-// import jakarta.validation.constraints.NotBlank;
-// import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-// public class UserDTO {
-//     @NotBlank(message = "name is required")
-// 	private String name;
-//     @NotBlank(message = "email is required")
-//     @Email(message="Invalid email format")
-//     private String email;
-//     @NotBlank(message = "password is required")
-//     private String password;
+public class UserDTO {
+
+    private Long id;
+    @NotBlank(message = "name is required")
+	private String name;
+    @NotBlank(message = "email is required")
+    @Email(message="Invalid email format")
+    private String email;
+    @NotBlank(message = "password is required")
+    private String password;
 	
-//     public String getName() {
-//         return this.name;
-//     }
-    
-//     public void setName(String name) {
-// 		this.name = name;
-// 	}
+    public Long getId() {
+        return this.id;
+    }
 
-//     @NotNull(message = "email cannot be NULL")
-//     public String getEmail() {
-//         return this.email;
-//     }
-    
-//     public void setEmail(String email) {
-// 	    this.email = email;
-// 	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-//     @NotNull(message = "password cannot be NULL")
-//     public String getPassword() {
-//         return this.password;
-//     }
-// }
+    public String getName() {
+        return this.name;
+    }
+    
+    public void setName(String name) {
+		this.name = name;
+	}
+
+    @NotNull(message = "email cannot be NULL")
+    public String getEmail() {
+        return this.email;
+    }
+    
+    public void setEmail(String email) {
+	    this.email = email;
+	}
+
+
+
+    @NotNull(message = "password cannot be NULL")
+    public String getPassword() {
+        return this.password;
+    }
+
+}
